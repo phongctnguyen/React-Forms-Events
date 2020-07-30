f677817f83fa9fc6322a6d02275ffd46d0c6d728
Nguyen Cao Truong Phong <phongctnguyen@gmail.com>
2020-07-30 23:47:58 +0700

    finish form and events basic section

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,9 +3,13 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 export default () => {
+  const onSearchSubmit = (term) => {
+    console.log(term)
+  }
+
   return (
     <div className='ui container' style={{ marginTop: '10px' }}>
-      <SearchBar />
+      <SearchBar onSubmit={onSearchSubmit}/>
     </div>
   );
 };
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-export default () => {
-  return (
-    <div className='ui segment'>
-      <form className='ui form'>
-        <div className='field'>
-          <label>Image Search</label>
-          <input type='text' onChange={(e) => console.log(e.target.value)} />
-        </div>
-      </form>
-    </div>
-  );
-};
+export default ({ onSubmit }) => {
+	const [term, setTerm] = useState('')
+
+	const onFormSubmit = (e) => {
+		e.preventDefault()
+
+    onSubmit(term);
+	}
+
+	return (
+		<div className='ui segment'>
+			<form className='ui form' onSubmit={onFormSubmit}>
+				<div className='field'>
+					<label>Image Search</label>
+					<input
+						type='text'
+						value={term}
+						onChange={(e) => setTerm(e.target.value)}
+					/>
+				</div>
+			</form>
+		</div>
+	)
+}
